327393355752b8a289ca732fcc3f078e412189c2
Jordi Dewit <jordi.dewit@student.hogent.be>
2021-11-06 16:16:02 +0100

    Red rectangle created.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -1,19 +1,63 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Jordi Dewit on 06/11/2021.
-//
-
 import SwiftUI
+ 
+ 
+ 
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+// This is the main view or the main box
+// a view is a input and output area
+struct ContentView: View //ContentView behaves like a view
+{
+    
+    var body: some View //body behaves also like another view = "some view"
+    {
+        RoundedRectangle(cornerRadius: 20.0)
+            .stroke(lineWidth: 3) //border with a width of 3px
+            .padding()            //padding
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)//color of border is red
+        
+     //   Text("Memorize").font(.largeTitle).padding()
     }
+    
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This shows our main view above
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
