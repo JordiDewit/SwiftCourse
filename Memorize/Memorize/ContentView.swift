e952036a6e2d1ceb52108b2e544a146b30bd001d
Jordi Dewit <jordi.dewit@student.hogent.be>
2021-11-06 16:59:08 +0100

    Les 1: rectangle and text in view blocks.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -1,17 +1,32 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Jordi Dewit on 06/11/2021.
-//
-
 import SwiftUI
+ 
+ 
+ //this is lesson 1
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+// This is the main view or the main box
+// a view is a input and output area
+struct ContentView: View //ContentView behaves like a view
+{
+    
+    var body: some View //body behaves also like another view = "some view"
+    {
+        ZStack // Puts all elements on top of eachother inside the zstack container
+        {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3) //border with a width of 3px
+      
+            
+            Text("🚗")
+                .font(.largeTitle)
+                .padding()
+        }
+        .padding()            //padding
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)//color of border is red
+       
+        
+     
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
