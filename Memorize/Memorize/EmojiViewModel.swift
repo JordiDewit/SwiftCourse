1015fc46d33b91be60f72e2e1fdb01dde575e171
Jordi Dewit <jordi.dewit@student.hogent.be>
2021-11-07 17:07:23 +0100

    Implemented the game logic: matching, observing changes..

--- Memorize/Memorize/EmojiViewModel.swift
+++ Memorize/Memorize/EmojiViewModel.swift
@@ -4,13 +4,13 @@ import Foundation
 
 
 
-class EmojiViewModel // observer for changes in the view(presentation of data) and model(storage of data)
+class EmojiViewModel: ObservableObject// observer for changes in the view(presentation of data) and model(storage of data)
 {
     //PROPERTIES
     static let emojis = ["🚗", "🚕", "🚌", "🚎","🏎","🚓","🚑","🛻","🚠","🛵","🚝","✈️","🚀", "🚇", "🛳", "🚁"] //you don't need to init to use this constant
     
-    private var model: MemoryGame<String> = createMemoryGame()
-    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
+    // @Published provides that every change will rebuild the view
     //String because this is the emoji version of memory game and only the code in this viewmodel can modify the model
     //You say there must be 4 pairs or 8 cards be made
     var cards: Array<MemoryGame<String>.Card>
@@ -23,10 +23,15 @@ class EmojiViewModel // observer for changes in the view(presentation of data) a
     
     static func createMemoryGame() -> MemoryGame<String>
     {
-        MemoryGame<String>(numberOfPairsOfCards: 4, createContent: { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 5, createContent: { pairIndex in
                   emojis[pairIndex]
             })
     }
     
+    //USER INTENTS
+    func choose(card: MemoryGame<String>.Card)
+    {
+        model.choose(card: card)
+    }
 
 }
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -1,16 +1,48 @@
 import Foundation
 import SwiftUI
 //This is the model
-struct MemoryGame<CardContent> //generic type for the content of MemoryGame.card
-{
+struct MemoryGame<CardContent> where CardContent: Equatable//generic type for the content of MemoryGame.card
+{ // Equatable provides you can't compare
     // attribute list of cards
     private(set) var cards: Array<Card> //only the choose function can modify the array
     
+    
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
+    
+    
+    
     //functions
-    func choose(card: Card)
+    mutating func choose(card: Card)
     {
-        
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),//checks or the chosenindex is not nil
+               !cards[chosenIndex].isFaceUp,//checks if chosen card is not already face up
+               !cards[chosenIndex].isMatched //checks if it is not already matched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard
+            {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content
+                {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            }else
+            {
+                for index in 0..<cards.count
+                {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+            //reverse value
+        }
     }
+    
+    
+    
     //init
     init(numberOfPairsOfCards: Int, createContent: (Int) -> CardContent)
     {
@@ -19,12 +51,19 @@ struct MemoryGame<CardContent> //generic type for the content of MemoryGame.card
         for pairIndex in 0..<numberOfPairsOfCards
         {
             let content: CardContent = createContent(pairIndex)
-            cards.append(Card(isFaceUp: false, isMatched: false, content: content))
-            cards.append(Card(isFaceUp: false, isMatched: false, content: content))
+            cards.append(Card(id: pairIndex*2, isFaceUp: false, isMatched: false, content: content))
+            cards.append(Card(id: pairIndex*2+1, isFaceUp: false, isMatched: false, content: content))
         }
+        cards.shuffle()
     }
-    struct Card //Card is a part of the memorygame
+    
+    
+    
+    //card
+    struct Card: Identifiable //Card is a part of the memorygame and behaves like a identifiable
     {
+        var id: Int // a unique id
+        
         var isFaceUp: Bool
         var isMatched: Bool
         var content: CardContent //generic or don't care type
